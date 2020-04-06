8f2edd745b27939d6b0fef259c42b20cce7b4e75
hrome <58020216+hromero7@users.noreply.github.com>
2020-04-05 21:02:16 -0700

    updating js file with working logic

--- script.js
+++ script.js
@@ -10,104 +10,78 @@ var upperBox = document.getElementById("uppercase-box");
 
 var numberBox = document.getElementById("number-box"); 
 
-var specialArr = "!@#$%^&*/<>?;~";
+var specialArr = ['!','@','#','$','%','^','&','*','/','<','>','?',';','~'];
 
-var uppercaseArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var uppercaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-var numberArr = "0123456789";
+var numberArr = ['0','1','2','3','4','5','6','7','8','9'];
 
-var lowercaseArr = uppercaseArr.toLowerCase(); 
+//var lowercaseArr = uppercaseArr.join(",");
 
-console.log(lowercaseArr); 
+
+var generatedPass = []; 
+var password = [];
+
+
 
 // functions
 
-var randomFunc = {
-    getSpecial: randomSpecial,
-    getUpper: randomUpper,
-    getNumber: randomNumber
-}; 
-
-function randomSpecial() {
-    return specialArr[Math.floor(Math.random() * specialArr.length)];
-    
-}
-
-function randomUpper() {
-    return uppercaseArr[Math.floor(Math.random() * uppercaseArr.length)];
-}
-
-function randomNumber() {
-    return numberArr[Math.floor(Math.random() * numberArr.length)]; 
-}
-
-function randomLower() {
-    return lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)];
-}
 
 
 
 generateEl.addEventListener("click", function(event) {
     event.preventDefault();
+    passwordEl.innerText = "";
     var passLength = prompt("Choose length of password. Min = 8, Max = 128");
+    var passwordLength = parseInt(passLength, 10); 
+    //for (var i = 0 ; i < passwordLength.value; i++) {
+        //password.push(generatedPass[Math.floor(Math.random() * generatedPass.length)]);
+       // passwordEl = passwordEl + generatedPass[length]; 
+    //}
 
-    //var specialValue = confirm("Include Special Characters?");
-
-    //var uppercaseValue = confirm("Include Uppercase Letters?");
-
-    //var numberValue = confirm("Include Numbers?");
-
-    var length = passLength.value; 
-    var special = specialBox.checked; 
-    var upper = upperBox.checked;
-    var number = numberBox.checked; 
+    if(passLength < 8) {
+        alert("Password must be longer than 8 characters!");
+        passwordEl.innerHTML = "";
+    } else if(passLength > 128){
+        alert("Length must be less than 128 characters!");
+        passwordEl.innerHTML = ""; 
+    } else{
     
-   
 
-    passwordEl.textContent = generatePass(length, special, upper, number); 
+    if (specialBox.checked === true) {
+        for (var i = 0; i < specialArr.length; i++) {
+            generatedPass.push(specialArr[i]);
+        }
+    }
+
+    if (upperBox.checked === true) {
+        for (var i = 0; i < uppercaseArr.length ; i++) {
+            generatedPass.push(uppercaseArr[i]);
+        }
+    }
+
+    if (numberBox.checked === true) {
+        for (var i = 0 ; i < numberArr.length; i++) {
+            generatedPass.push(numberArr[i]); 
+        }
+     }
+     
+     for (var i = 0 ; i < passwordLength; i++) {
+        password.push(generatedPass[Math.floor(Math.random() * generatedPass.length)]);
+        //passwordEl = passwordEl + generatedPass[length]; 
+    }
+    //  Use generatedPass to create a password using .random and the passwordLength
+    //password.push(generatedPass[Math.floor(Math.random() * generatedPass.length)]);
+
+
+    passwordEl.textContent = password.join(''); 
+
+    
+    console.log(passwordLength); 
+
+    }
+
 
 
 });
-
-function generatePass(length, getSpecial, getUpper, getNumber) {
-    var generatedPass = "";
-
-    var boxCount = length + getSpecial + getUpper + getNumber;
-
-    var criteria = [{length}, {getSpecial}, {getUpper}, {getNumber}].filter(item => Object.values(item)[0]);
-
- 
-
-   if (boxCount === 0) {
-     return "";
-    }
-
-    for(var i = 0; i < length; i += boxCount) {
-        criteria.forEach(type => {
-            var funcName = Object.keys(type)[0];
-
-            
-
-            generatedPass += randomFunc[funcName](); 
-        });
-    }
-
-    var generatedFinal = generatedPass.slice(0, length); 
-    return generatedFinal; 
-
-    
-    
-}
-
-
-
-
-    
-
-
-
-
-
-
-
 
